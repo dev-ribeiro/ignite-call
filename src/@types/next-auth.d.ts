37e3df9eaca2d0bfc4c5faebe44a54ebeadeccf4
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-01-14 21:06:32 -0300

    lesson: create next adapter

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import nextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  export interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
+}
