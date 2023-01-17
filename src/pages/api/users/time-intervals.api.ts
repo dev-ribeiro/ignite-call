544a6d9674047083b442f87030043a0a90d4e6e7
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-01-16 22:20:35 -0300

    lesson: persisting intervals in db

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,7 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import * as z from 'zod'
 // eslint-disable-next-line camelcase
 import { unstable_getServerSession } from 'next-auth/next'
 import { buildNextAuthOption } from '../auth/[...nextauth].api'
+import { prisma } from '../../../lib/prisma'
+
+const timeIntervalBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,5 +29,24 @@ export default async function handler(
     buildNextAuthOption(req, res),
   )
 
-  return res.status(201).json({ session })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
