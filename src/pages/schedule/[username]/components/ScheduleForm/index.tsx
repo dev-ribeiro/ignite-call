8b556a61e85182582c146a9c59655a564c7ea4cd
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-01-28 10:22:44 -0300

    lesson: create confirm schedule page

--- src/pages/schedule/[username]/components/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/components/ScheduleForm/index.tsx
@@ -1,10 +1,10 @@
-import { CalendarStep } from '../CalendarStep'
+// import { CalendarStep } from '../CalendarStep'
+import { ConfirmStep } from '../ConfirmStep'
 // import { Container } from './styles'
 
 export function ScheduleForm() {
   return (
-    // <Container>
-    <CalendarStep />
-    // </Container>
+    // <CalendarStep />
+    <ConfirmStep />
   )
 }
