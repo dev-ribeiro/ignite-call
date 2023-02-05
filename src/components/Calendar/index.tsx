b637f32b294b1863f26a6dcf3a42458bfab584bc
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-05 20:54:24 -0300

    lesson: navigate in months

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,4 +1,6 @@
+import dayjs from 'dayjs'
 import { CaretLeft, CaretRight } from 'phosphor-react'
+import { useState } from 'react'
 import { getWeekDays } from '../../utils/getWeekDays'
 import {
   CalendarActions,
@@ -10,19 +12,35 @@ import {
 } from './styles'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
+  function handlePreviusMonth() {
+    const previusMonthData = currentDate.subtract(1, 'month')
+    setCurrentDate(previusMonthData)
+  }
+  function handleNextMonth() {
+    const nextMonthData = currentDate.add(1, 'month')
+    setCurrentDate(nextMonthData)
+  }
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Janeiro <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviusMonth} title="Previus month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
--- src/pages/schedule/[username]/components/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/components/ScheduleForm/index.tsx
@@ -1,10 +1,10 @@
-// import { CalendarStep } from '../CalendarStep'
-import { ConfirmStep } from '../ConfirmStep'
+import { CalendarStep } from '../CalendarStep'
+// import { ConfirmStep } from '../ConfirmStep'
 // import { Container } from './styles'
 
 export function ScheduleForm() {
   return (
-    // <CalendarStep />
-    <ConfirmStep />
+    <CalendarStep />
+    // <ConfirmStep />
   )
 }
