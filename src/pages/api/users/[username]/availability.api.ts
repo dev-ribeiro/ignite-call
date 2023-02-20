3e083ac9a8b815c85e7656f5dc12f625fbde8879
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-20 18:53:46 -0300

    lesson: create api route of availability

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,60 @@
+/* eslint-disable camelcase */
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  const userAvalaibility = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvalaibility) {
+    return res.json({ availability: [] })
+  }
+
+  const { time_end_in_minutes, time_start_in_minutes } = userAvalaibility
+
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, index) => {
+      return startHour + index
+    },
+  )
+
+  return res.json({ possibleTimes })
+}
